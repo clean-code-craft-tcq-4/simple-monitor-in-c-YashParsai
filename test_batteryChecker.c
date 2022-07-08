8f02f72316959e1890b1a6ef3c8e17a070a1bb06
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-07-08 15:25:57 +0530

    Update test_batteryChecker.c

--- test_batteryChecker.c
+++ test_batteryChecker.c
@@ -1,7 +1,7 @@
 #include <assert.h>
 #include "batteryChecker.h"
 
-float DummyInputParam[6][3] = { {20,70,0.2},{50,60,0.7},{30,60,1},{20,30,2},{50,10,1},{30,45,6} };
+float DummyInputParam[6][3] = { {20,70,2},{50,60,0.7},{30,60,1},{20,30,2},{50,10,1},{30,45,6} };
 
 void test_battery(void)
 {
@@ -13,7 +13,7 @@ void test_battery(void)
                 batteryParam.temp = DummyInputParam[i][0];
                 batteryParam.soc = DummyInputParam[i][1];
                 batteryParam.chargeRate = DummyInputParam[i][2];
-                assert(batteryIsOk(batteryParam));
+                assert(!batteryIsOk(batteryParam));
         }
 
 }
